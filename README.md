9db6e5b16c1734109580980051f02667049486aa
Christopher Moody <chrisemoody@gmail.com>
2017-10-15 21:15:08 -0700

    Improve coefs on MFDeep1 model

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# simple_mf
+# pytorch-rec
 Matrix factorization models in pytorch
 
 Install steps
 
-1. You'll need to install pytorch
+1. You'll need to install [pytorch](http://pytorch.org/) seperately
 2. pip3 -r requirements.txt
-3. python3 download.py
+3. python3 download.py  # This will take 1-2Gb of disk space
 4. python3 run.py
 
 Included models:
--- run_matrix.py
+++ run_matrix.py
@@ -19,52 +19,35 @@ dim = 16
 n_epochs = 40
 batchsize = 4096
 model_type = 'MF'
-learning_rate = 1e-3  # 1e-3 is standard
+learning_rate = 1e-2  # 1e-3 is standard
 fn = model_type + '_checkpoint'
 
-n_items = np.load('full.npz')['n_items'].tolist()
-n_users = np.load('full.npz')['n_users'].tolist()
-n_colr = np.load('full.npz')['n_colr'].tolist()
-n_feat = np.load('full.npz')['n_feat'].tolist()
+n_item = np.load('full.npz')['n_items'].tolist()
+n_user = np.load('full.npz')['n_users'].tolist()
 n_obs = np.load('full.npz')['n_obs'].tolist()
 seed = np.load('full.npz')['seed'].tolist()
 
 train_item = np.load('train.npz')['item'].astype('int64')
 train_user = np.load('train.npz')['user'].astype('int64')
-train_uage = np.load('train.npz')['uage'].astype('float32')
-train_like = np.load('train.npz')['like'].astype('float32')
-train_colr = np.load('train.npz')['colr'].astype('int64')
-train_feat = np.load('train.npz')['feat'].astype('int64')
+train_scor = np.load('train.npz')['feat'].astype('float32')
 
 test_item = np.load('test.npz')['item'].astype('int64')
 test_user = np.load('test.npz')['user'].astype('int64')
-test_uage = np.load('test.npz')['uage'].astype('float32')
-test_like = np.load('test.npz')['like'].astype('float32')
-test_colr = np.load('test.npz')['colr'].astype('int64')
-test_feat = np.load('test.npz')['feat'].astype('int64')
+test_scor = np.load('test.npz')['uage'].astype('float32')
 
 callbacks = {'auc': auc_callback}
 if model_type == 'MF':
-    model = MF(n_users, n_items, dim, n_obs, luv=1e-3,
+    model = MF(n_user, n_item, dim, n_obs, luv=1e-3,
                lub=1e-3, liv=1e-3, lib=1e-3)
-    train_args = (train_user, train_item, train_like)
-    test_args = (test_user, test_item, test_like)
-elif model_type == 'MFPoly2':
-    model = MFPoly2(n_users, n_items, dim, n_obs, luv=1e-3,
-                    lub=1e-3, liv=1e-3, lib=1e-3)
-    train_args = (train_user, train_item, train_uage, train_like)
-    test_args = (test_user, test_item, test_uage, test_like)
+    train_args = (train_user, train_item, train_scor)
+    test_args = (test_user, test_item, test_scor)
 elif model_type == 'MFDeep1':
-    model = MFDeep1(n_users, n_items, dim, n_obs, luv=1e-3,
-                    lub=1e-3, liv=1e-3, lib=1e-3)
-    train_args = (train_user, train_item, train_like)
-    test_args = (test_user, test_item, test_like)
-elif model_type == 'MFPoincare':
-    model = MFPoincare(n_users, n_items, dim, n_obs)
-    train_args = (train_user, train_item, train_like)
-    test_args = (test_user, test_item, test_like)
+    model = MFDeep1(n_user, n_item, dim, n_obs, luv=1e-6,
+                    lub=1e-6, liv=1e-6, lib=1e-6)
+    train_args = (train_user, train_item, train_scor)
+    test_args = (test_user, test_item, test_scor)
 elif model_type == 'FM':
-    n_feat = n_items + n_users + n_colr
+    n_feat = n_item + n_user
     model = FM(n_feat, dim, n_obs, lb=1e-6, lv=1e-6)
     train_args = (train_feat, train_like)
     test_args = (test_feat, test_like)
